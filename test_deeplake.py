f191cf7dfcd761cb8aba5efe20bbb73bbc47d3ca
polina tufanova <ptufanova@gmail.com>
2025-06-03 14:41:45 +0100

    some changes

--- test_deeplake.py
+++ test_deeplake.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
 
-os.environ["ACTIVELOOP_TOKEN"] = "eyJhbGciOiJIUzUxMiIsImlhdCI6MTc0ODk1MTUxNiwiZXhwIjoxNzgwNDg3NDk1fQ.eyJpZCI6ImphbW15bmluamE5NSIsIm9yZ19pZCI6ImphbW15bmluamE5NSJ9.thC8cfrTgZJSN_1D4-rnd_dLjBTMPanrHshiAUavzXX9OyB94AofbbFIAnNOalsYVRhsGw87C9I0j9C_vZ4X_A"
-
+os.environ["ACTIVELOOP_TOKEN"] = ""
 
 ds = deeplake.load("hub://activeloop/kth-actions")
 # dataloader = ds.pytorch(num_workers=0, batch_size=4, shuffle=False)
