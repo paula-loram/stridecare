a1d98f612042e488c719a2d3e71778684fccdb65
Paula Lopez Ramos <paula_lopezr@hotmail.com>
2025-06-09 17:21:58 +0100

    working on fastapi and docker

---
+++ stridecare/preprocessing.py
@@ -0,0 +1,6 @@
+#should have the preprocessing of input data -- vid to angle
+#pickled scaler and ohe
+#and here is where we calculate the prediction with the model
+#import final_mediapipe_angles to have get_mediapipe_angles() available
+
+
